83632d7ec09fabb632611ea19441b3dbdb1f2878
kmezhoud <kmezhoud>
2018-05-13 20:48:08 +0100

    save CNN model as data (1)

--- R/papSmear.R
+++ R/papSmear.R
@@ -21,5 +21,17 @@ papSmear <- function(){
   if (!"package:papSmear" %in% search())
     if (!require(papSmear)) stop("Calling papSmear start function but papSmear is not installed.")
     runApp(system.file("app", package = "papSmear"), launch.browser = TRUE)
-    
-  }
+}
+
+#' CNN
+#'
+#' @name CNN
+#' @description Trained Convolution Neuronal Network on images of unique cervix cells
+#'              The model is build with mxnet package using 2 convolution  and 2 fully connected layers
+#'
+#' @details 90 rounds, 20 images per round, 7 folders that countain 7 cells classes
+#' @docType data
+#' @author Karim Mezhoud \email{kmezhoud@gmail.com}
+#' @usage data(CNN)
+#' @keywords data
+NULL
